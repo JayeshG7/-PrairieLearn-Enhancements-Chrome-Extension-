ee70191ac128047bfdeebe44db95e6bb341477fb
Pedro Coelho <113377096+korokkun@users.noreply.github.com>
2024-03-08 16:24:50 -0600

    Separated code into background and content scripts; listener is added automatically through manifest.json so I simplified the background code, and put code in the content script that scrapes the relevant links and prints them to console

---
+++ main/chrome_extension/src/background/trigger.ts
@@ -0,0 +1,26 @@
+/**
+* Basic chrome extension sample code
+*/
+// chrome.action.onClicked.addListener((tab) => {
+//   // Ensure there is a valid tab.id before trying to send a message to the content script
+//   if (tab.id !== undefined) {
+//       chrome.scripting.executeScript({
+//           target: { tabId: tab.id },
+//           files: ['src/content/content.js'] // Ensure this path matches where your contentScript.js is located
+//       }, () => {
+//           // After the content script is injected, send it a message to start extracting URLs
+//           if (tab.id != undefined) {
+//             chrome.tabs.sendMessage(tab.id, {action: "extractURLs"});
+//           }
+//       });
+//   }
+// });
+
+chrome.action.onClicked.addListener((tab) => {
+  // Ensure there is a valid tab.id before trying to send a message to the content script
+    if (tab.id != undefined) {
+      // Send a message to extract URLs. The content script has already been injected in the 
+      // webpage through the manifest.json file so no need to do it again
+      chrome.tabs.sendMessage(tab.id, {action: "extractURLs"});
+    }
+});
---
+++ main/chrome_extension/src/content/content.ts
@@ -0,0 +1,36 @@
+(function() {
+
+  function getClassURLs() {
+    console.log('getClassURLs called!');
+    // Use querySelectorAll to find all <a> tags in the document
+    // This is because the links to the classes we are signed up to in PrairieLearn are all stored
+    // in this format in the home page
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+    let aTags = document.querySelectorAll('a');
+  
+    // Initialize an array to store the values of these links
+    let hrefs: string[] = [];
+  
+    // Iterate over the NodeList of <a> tags
+    aTags.forEach(aTag => {
+      // Extract the href attribute of each <a> tag, if it exists
+      // This is where the link is stored
+      let href = aTag.getAttribute('href');
+      if (href !== null) { // Ensure the href is not null
+        if (href.includes("course_instance")) {
+          hrefs.push("https://us.prairielearn.com/" + href);
+        }
+      }
+    });
+  
+    console.log('Extracted hrefs:', hrefs);
+    return hrefs; // Return the array of href values
+  }
+  
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "extractURLs") {
+        getClassURLs();
+    }
+  });
+
+})();
