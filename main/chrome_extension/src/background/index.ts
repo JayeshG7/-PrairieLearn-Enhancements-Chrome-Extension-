e7a653b145be4c62eec72253adcdd434a72d09b9
Pedro Coelho <113377096+korokkun@users.noreply.github.com>
2024-03-08 12:10:44 -0600

    Got href extraction almost working, need to separate into content and background scripts because background scripts do not exist in the context of a webpage, so it cannot access DOMParser

--- main/chrome_extension/src/background/index.ts
+++ main/chrome_extension/src/background/index.ts
@@ -3,27 +3,13 @@
 */
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
-    //   func: () => {
-    //     const myButton: HTMLButtonElement = document.createElement('button');
-    //     myButton.textContent = 'Say hi to Pedro!';
-    //     myButton.onclick = () => {
-    //       alert('Hey Pedro!')
-    //     }
-    //     document.body.appendChild(myButton);
-    //   },
-    //   target: {
-    //     tabId: tab.id || 0
-    //   }
-    // }).then(() => {
-    //   console.log('Button inserted');
-    // }).catch((err) => {
-    //   console.error('Button not inserted', err);
-    // });
     func: () => {
       // Get the current DOM and serialize it into a string
       const domContent = new XMLSerializer().serializeToString(document);
       // Paste the string onto the terminal
-      console.log(domContent);
+      // console.log(domContent);
+      // Send the data to where it needs to go
+      chrome.runtime.sendMessage({domContent: domContent})
     },
     target: {
       tabId: tab.id || 0
@@ -35,3 +21,45 @@ chrome.action.onClicked.addListener((tab) => {
   });
 })
 
+chrome.runtime.onMessage.addListener(
+  (message, sender, sendResponse) => {
+    if (message.domContent) {
+      // Perform processing on the DOM content that was received
+      console.log('DOM content received!');
+      getClassURLs(message.domContent);
+    }
+  }
+);
+
+function getClassURLs(domContent: string): string[] {
+  console.log('getClassURLs called!');
+  // Define a new instance of DOMParser, a JS library for parsing DOM content (like BeautifulSoup)
+  // https://developer.mozilla.org/en-US/docs/Web/API/DOMParser
+  let parser = new DOMParser();
+  
+  // The domContent is a string, but to be able to use some of the DOMParser functions we want
+  // like querySelector, we need to call parseFromString to turn it into a doc
+  let doc = parser.parseFromString(domContent, 'text/html');
+  
+  // Use querySelectorAll to find all <a> tags in the document
+  // This is because the links to the classes we are signed up to in PrairieLearn are all stored
+  // in this format in the home page
+  // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+  let aTags = doc.querySelectorAll('a');
+
+  // Initialize an array to store the values of these links
+  let hrefs: string[] = [];
+
+  // Iterate over the NodeList of <a> tags
+  aTags.forEach(aTag => {
+    // Extract the href attribute of each <a> tag, if it exists
+    // This is where the link is stored
+    let href = aTag.getAttribute('href');
+    if (href !== null) { // Ensure the href is not null
+      hrefs.push("https://us.prairielearn.com/" + href);
+    }
+  });
+
+  console.log('Extracted hrefs:', hrefs);
+  return hrefs; // Return the array of href values
+}
